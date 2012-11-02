ff899f90e94ee1dadb784b83b067ff87dd02f0cd
Erwin Mueller <erwin.mueller@deventm.org>
2012-11-02 12:44:14 +0100

    Add missing license headers

--- sscontrol-workers-command/src/main/java/com/anrisoftware/sscontrol/workers/command/exec/service/LazyInjector.java
+++ sscontrol-workers-command/src/main/java/com/anrisoftware/sscontrol/workers/command/exec/service/LazyInjector.java
@@ -1,7 +1,20 @@
 /*
  * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of noisedriver-crbsnoise. All rights reserved.
+ * This file is part of sscontrol-workers-command.
+ *
+ * sscontrol-workers-command is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-workers-command is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-workers-command. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.workers.command.exec.service;
 
@@ -15,7 +28,8 @@ import com.google.inject.Injector;
 import com.google.inject.Module;
 
 /**
- * Lazy create the injector with the needed modules for the template service.
+ * Lazy create the injector with the needed modules for the execute command
+ * service.
  * 
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 0.1

e47af4e77041167af8c84bfdf4c56e46ed9f0064
Erwin Mueller <erwin.mueller@deventm.org>
2013-02-10 17:53:41 +0100

    Fix hosts parser tests
    Use semicolon to separate the hosts attributes, because the comma is
    used to separate the hosts.
    #2125

--- sscontrol-hosts/src/test/groovy/com/anrisoftware/sscontrol/hosts/utils/HostFormatTest.groovy
+++ sscontrol-hosts/src/test/groovy/com/anrisoftware/sscontrol/hosts/utils/HostFormatTest.groovy
@@ -6,6 +6,7 @@ import groovy.util.logging.Slf4j
 import org.junit.BeforeClass
 import org.junit.Test
 
+import com.anrisoftware.sscontrol.hosts.service.Host
 import com.anrisoftware.sscontrol.hosts.service.HostFactory
 import com.anrisoftware.sscontrol.hosts.service.HostsModule
 import com.google.inject.Guice
@@ -21,35 +22,31 @@ class HostFormatTest {
 			[address: "127.0.0.1", name: "localhost", aliases: ["localdomain", "localhost"]],
 		],
 		outputs: [
-			'address:"127.0.0.1",name:"localhost",aliases:[]',
-			'address:"127.0.0.1",name:"localhost",aliases:["localdomain"]',
-			'address:"127.0.0.1",name:"localhost",aliases:["localdomain","localhost"]',
+			'address:"127.0.0.1";name:"localhost";aliases:[]',
+			'address:"127.0.0.1";name:"localhost";aliases:["localdomain"]',
+			'address:"127.0.0.1";name:"localhost";aliases:["localdomain","localhost"]',
 		]
 	]
 
 	@Test
 	void "format host"() {
 		inputsOutputsFormat.inputs.eachWithIndex { it, int i ->
-			def host = hostFactory.create(it.address).hostname(it.name).alias(it.aliases)
+			def host = hostFactory.create(it.address)
+			host.host(it.name).alias(it.aliases)
 			def format = factory.create().format(host)
 			log.info "Format: '{}'.", format
 			assertStringContent format, inputsOutputsFormat.outputs[i]
 		}
 	}
 
-	static inputsOutputsParse = [
-		inputs: [
-			'address:"127.0.0.1",name:"localhost",aliases:[]',
-			'address:"127.0.0.1",name:"localhost",aliases:["localdomain"]',
-			'address:"127.0.0.1",name:"localhost",aliases:["localdomain","localhost"]',
-		]]
-
 	@Test
 	void "parse host"() {
-		inputsOutputsParse.inputs.eachWithIndex { it, int i ->
-			def parsed = factory.create().parse(it)
+		inputsOutputsFormat.outputs.eachWithIndex { it, int i ->
+			Host parsed = factory.create().parse(it)
 			log.info "Parsed: '{}'.", parsed
-			assert parsed == inputsOutputsParse.outputs[i]
+			assert parsed.address == inputsOutputsFormat.inputs[i].address
+			assert parsed.hostname == inputsOutputsFormat.inputs[i].name
+			assert parsed.aliases == inputsOutputsFormat.inputs[i].aliases
 		}
 	}
 
@@ -64,12 +61,6 @@ class HostFormatTest {
 		injector = Guice.createInjector(new HostsModule())
 		factory = injector.getInstance(HostFormatFactory)
 		hostFactory = injector.getInstance(HostFactory)
-		inputsOutputsParse << [outputs: [
-				hostFactory.create("127.0.0.1").hostname("localhost").alias(),
-				hostFactory.create("127.0.0.1").hostname("localhost").alias("localdomain"),
-				hostFactory.create("127.0.0.1").hostname("localhost").alias("localdomain","localhost"),
-			]
-		]
 	}
 
 	static {

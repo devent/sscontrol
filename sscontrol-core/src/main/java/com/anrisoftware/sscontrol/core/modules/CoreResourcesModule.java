7f4447f8dfda491c77117a554f54ef3fbf88c26b
Erwin Mueller <erwin.mueller@deventm.org>
2013-07-22 22:38:35 +0200

    Add template resources
    #2506

--- sscontrol-core/src/main/java/com/anrisoftware/sscontrol/core/modules/CoreResourcesModule.java
+++ sscontrol-core/src/main/java/com/anrisoftware/sscontrol/core/modules/CoreResourcesModule.java
@@ -1,28 +1,28 @@
 /*
  * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
- *
+ * 
  * This file is part of sscontrol-core.
- *
+ * 
  * sscontrol-core is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- *
- * sscontrol-core is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
- *
+ * 
+ * sscontrol-core is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Affero General Public License
  * along with sscontrol-core. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.core.modules;
 
-import com.anrisoftware.resources.binary.binaries.BinariesResourcesModule;
-import com.anrisoftware.resources.binary.maps.BinariesDefaultMapsModule;
-import com.anrisoftware.resources.texts.maps.TextsDefaultMapsModule;
-import com.anrisoftware.resources.texts.texts.TextsResourcesCharsetModule;
-import com.anrisoftware.resources.texts.texts.TextsResourcesModule;
+import com.anrisoftware.resources.templates.maps.TemplatesDefaultMapsModule;
+import com.anrisoftware.resources.templates.templates.TemplatesResourcesModule;
+import com.anrisoftware.resources.templates.worker.STDefaultPropertiesModule;
+import com.anrisoftware.resources.templates.worker.STWorkerModule;
+import com.anrisoftware.resources.texts.defaults.TextsResourcesDefaultModule;
 import com.google.inject.AbstractModule;
 
 /**
@@ -39,10 +39,10 @@ public class CoreResourcesModule extends AbstractModule {
 	}
 
 	private void installResources() {
-		install(new TextsResourcesModule());
-		install(new TextsDefaultMapsModule());
-		install(new TextsResourcesCharsetModule());
-		install(new BinariesResourcesModule());
-		install(new BinariesDefaultMapsModule());
+		install(new TextsResourcesDefaultModule());
+		install(new TemplatesResourcesModule());
+		install(new TemplatesDefaultMapsModule());
+		install(new STWorkerModule());
+		install(new STDefaultPropertiesModule());
 	}
 }

29e8496b245260abf3b91747733cbe3cd549eb7c
Erwin Mueller <erwin.mueller@deventm.org>
2013-08-11 15:02:08 +0200

    Better logging for exec command worker
    #2607

--- sscontrol-workers-command/src/main/java/com/anrisoftware/sscontrol/workers/command/exec/ExecCommandWorkerLogger.java
+++ sscontrol-workers-command/src/main/java/com/anrisoftware/sscontrol/workers/command/exec/ExecCommandWorkerLogger.java
@@ -1,20 +1,21 @@
 /*
  * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
+ * 
  * This file is part of sscontrol-workers-command.
- *
- * sscontrol-workers-command is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * sscontrol-workers-command is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
- *
+ * 
+ * sscontrol-workers-command is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * sscontrol-workers-command is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Affero General Public License
- * along with sscontrol-workers-command. If not, see <http://www.gnu.org/licenses/>.
+ * along with sscontrol-workers-command. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.workers.command.exec;
 
@@ -31,13 +32,15 @@ import com.anrisoftware.sscontrol.workers.api.WorkerException;
  */
 class ExecCommandWorkerLogger extends AbstractLogger {
 
-	private static final String EXECUTE_COMMAND_INFO = "Execute command '{}'.";
 	private static final String EXECUTE_COMMAND = "Execute command {}.";
-	private static final String FINISH_EXECUTE_COMMAND_INFO = "Finished execute command '{}'.";
-	private static final String FINISH_EXECUTE_COMMAND = "Finished execute command {}.";
-	private static final String ERROR_EXECUTE_COMMAND_MESSAGE = "Error execute command '{}'.";
+	private static final String FINISH_EXECUTE_COMMAND = "Finished execute command {} code={}, <<<\n{}\n<<<EOL, out= <<<\n{}<<<EOL, err= <<<\n{}<<<EOL";
+	private static final String ERROR_EXECUTE_COMMAND_MESSAGE = "Error execute command <<<\n{}\n<<<EOL";
 	private static final String WORKER = "worker";
 	private static final String ERROR_EXECUTE_COMMAND = "Error execute command";
+	private static final String EXIT_CODE = "exit code";
+	private static final String OUT = "output";
+	private static final String ERR = "error output";
+	private static final String COMMAND = "command";
 
 	/**
 	 * Create logger for {@link ExecCommandWorker}.
@@ -47,24 +50,20 @@ class ExecCommandWorkerLogger extends AbstractLogger {
 	}
 
 	WorkerException errorExecuteCommand(ExecCommandWorker worker, IOException e) {
-		return logException(new WorkerException(ERROR_EXECUTE_COMMAND, e).add(
-				WORKER, worker), ERROR_EXECUTE_COMMAND_MESSAGE,
-				worker.getCommand());
+		return logException(
+				new WorkerException(ERROR_EXECUTE_COMMAND, e)
+						.add(WORKER, worker).add(COMMAND, worker.getCommand())
+						.add(EXIT_CODE, worker.getExitCode())
+						.add(OUT, worker.getOut()).add(ERR, worker.getErr()),
+				ERROR_EXECUTE_COMMAND_MESSAGE, worker.getCommand());
 	}
 
 	void finishedProcess(ExecCommandWorker worker) {
-		if (log.isDebugEnabled()) {
-			log.debug(FINISH_EXECUTE_COMMAND, worker);
-		} else {
-			log.info(FINISH_EXECUTE_COMMAND_INFO, worker.getCommand());
-		}
+		log.debug(FINISH_EXECUTE_COMMAND, worker, worker.getExitCode(),
+				worker.getCommand(), worker.getOut(), worker.getErr());
 	}
 
 	void startProcess(ExecCommandWorker worker) {
-		if (log.isDebugEnabled()) {
-			log.debug(EXECUTE_COMMAND, worker);
-		} else {
-			log.debug(EXECUTE_COMMAND_INFO, worker.getCommand());
-		}
+		log.debug(EXECUTE_COMMAND, worker);
 	}
 }

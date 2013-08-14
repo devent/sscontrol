7d6eae9b6ec47d17c41fd1bcae763633b3c6d8cf
Erwin Mueller <erwin.mueller@deventm.org>
2013-08-14 17:45:53 +0200

    Null environment means no environment was set
    fixes #2618

--- sscontrol-workers-command/src/main/java/com/anrisoftware/sscontrol/workers/command/exec/ExecCommandWorker.java
+++ sscontrol-workers-command/src/main/java/com/anrisoftware/sscontrol/workers/command/exec/ExecCommandWorker.java
@@ -1,20 +1,21 @@
 /*
  * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
+ * 
  * This file is part of sscontrol-workers-command.
- *
- * sscontrol-workers-command is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * sscontrol-workers-command is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
- *
+ * 
+ * sscontrol-workers-command is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * sscontrol-workers-command is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Affero General Public License
- * along with sscontrol-workers-command. If not, see <http://www.gnu.org/licenses/>.
+ * along with sscontrol-workers-command. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.workers.command.exec;
 
@@ -223,6 +224,10 @@ public class ExecCommandWorker implements Worker {
 
 	@Override
 	public String toString() {
-		return new ToStringBuilder(this).append(environment).toString();
+		ToStringBuilder builder = new ToStringBuilder(this);
+		if (environment != null) {
+			builder.append("environment", environment);
+		}
+		return builder.toString();
 	}
 }

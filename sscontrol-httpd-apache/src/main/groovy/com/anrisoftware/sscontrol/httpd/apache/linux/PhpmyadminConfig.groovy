293e8187d7fb1ebc24d6fc51533e367dafc993d9
Erwin Mueller <erwin.mueller@deventm.org>
2013-09-04 15:25:13 +0200

    Update license headers
    #2656

--- sscontrol-httpd-apache/src/main/groovy/com/anrisoftware/sscontrol/httpd/apache/linux/PhpmyadminConfig.groovy
+++ sscontrol-httpd-apache/src/main/groovy/com/anrisoftware/sscontrol/httpd/apache/linux/PhpmyadminConfig.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of sscontrol-httpd-apache.
+ *
+ * sscontrol-httpd-apache is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-httpd-apache is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-httpd-apache. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.sscontrol.httpd.apache.linux
 
 import com.anrisoftware.sscontrol.httpd.statements.domain.Domain

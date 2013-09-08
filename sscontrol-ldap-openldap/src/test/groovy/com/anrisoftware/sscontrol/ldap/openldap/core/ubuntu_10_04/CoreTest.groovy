35d4bfca38a7a3b5f969ddacbe795d3147ccf098
Erwin Mueller <erwin.mueller@deventm.org>
2013-09-08 14:32:48 +0200

    Update license headers
    #2660

--- sscontrol-ldap-openldap/src/test/groovy/com/anrisoftware/sscontrol/ldap/openldap/core/ubuntu_10_04/CoreTest.groovy
+++ sscontrol-ldap-openldap/src/test/groovy/com/anrisoftware/sscontrol/ldap/openldap/core/ubuntu_10_04/CoreTest.groovy
@@ -1,20 +1,20 @@
 /*
  * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of sscontrol-httpd-apache.
+ * This file is part of sscontrol-ldap-openldap.
  *
- * sscontrol-httpd-apache is free software: you can redistribute it and/or modify it
+ * sscontrol-ldap-openldap is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * sscontrol-httpd-apache is distributed in the hope that it will be useful, but
+ * sscontrol-ldap-openldap is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with sscontrol-httpd-apache. If not, see <http://www.gnu.org/licenses/>.
+ * along with sscontrol-ldap-openldap. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.ldap.openldap.core.ubuntu_10_04
 
--- sscontrol-ldap/src/test/groovy/com/anrisoftware/sscontrol/ldap/service/LdapTestUtil.groovy
+++ sscontrol-ldap/src/test/groovy/com/anrisoftware/sscontrol/ldap/service/LdapTestUtil.groovy
@@ -1,20 +1,20 @@
 /*
  * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of sscontrol-httpd.
+ * This file is part of sscontrol-ldap.
  *
- * sscontrol-httpd is free software: you can redistribute it and/or modify it
+ * sscontrol-ldap is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * sscontrol-httpd is distributed in the hope that it will be useful, but
+ * sscontrol-ldap is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with sscontrol-httpd. If not, see <http://www.gnu.org/licenses/>.
+ * along with sscontrol-ldap. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.ldap.service
 

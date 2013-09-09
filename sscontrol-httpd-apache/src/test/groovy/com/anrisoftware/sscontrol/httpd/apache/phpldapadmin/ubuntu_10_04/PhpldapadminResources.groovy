8a402c489972ea8581182af5faa9d2db6ac41591
Erwin Mueller <erwin.mueller@deventm.org>
2013-09-09 20:02:41 +0200

    Create phpldapadmin tests
    #2659

---
+++ sscontrol-httpd-apache/src/test/groovy/com/anrisoftware/sscontrol/httpd/apache/phpldapadmin/ubuntu_10_04/PhpldapadminResources.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of sscontrol-httpd-apache.
+ *
+ * sscontrol-httpd-apache is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-httpd-apache is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-httpd-apache. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.sscontrol.httpd.apache.phpldapadmin.ubuntu_10_04
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static org.apache.commons.io.FileUtils.*
+
+import com.anrisoftware.sscontrol.httpd.apache.core.ubuntu_10_04.ResourcesUtils
+
+/**
+ * Loads phpldapadmin/resources.
+ *
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+enum PhpldapadminResources {
+
+	httpdScript("Httpd.groovy", PhpldapadminResources.class.getResource("HttpdPhpldapadmin.groovy")),
+	domainsConf("/etc/apache2/conf.d/000-robobee-domains.conf", PhpldapadminResources.class.getResource("domains_conf.txt")),
+	test1comConf("/etc/apache2/sites-available/100-robobee-test1.com.conf", PhpldapadminResources.class.getResource("test1_com_conf.txt")),
+	test1comSslConf("/etc/apache2/sites-available/100-robobee-test1.com-ssl.conf", PhpldapadminResources.class.getResource("test1_com_ssl_conf.txt")),
+	ldapadminTest1comSslConf("/etc/apache2/sites-available/100-robobee-ldapadmin.test1.com-ssl.conf", PhpldapadminResources.class.getResource("ldapadmin_test1_com_ssl_conf.txt")),
+	ldapadminTest1comSslFcgiScript("/var/www/php-fcgi-scripts/ldapadmin.test1.com/php-fcgi-starter", PhpldapadminResources.class.getResource("php_fcgi_starter.txt")),
+	chownOut("/bin/chown.out", PhpldapadminResources.class.getResource("chown_out.txt")),
+	chmodOut("/bin/chmod.out", PhpldapadminResources.class.getResource("chmod_out.txt")),
+	phpldapadminTgz("/tmp/phpldapadmin-1.2.3.tgz", PhpldapadminResources.class.getResource("phpldapadmin-1.2.3.tgz")),
+	exampleConfig("/var/www/ldapadmin.test1.com/phpldapadmin-1.2.3/config/config.php.example", PhpldapadminResources.class.getResource("config_php_example.txt")),
+	linkedExampleConfig("/var/www/ldapadmin.test1.com/phpldapadmin/config/config.php.example", PhpldapadminResources.class.getResource("config_php_example.txt")),
+	expectedConfig("/var/www/ldapadmin.test1.com/phpldapadmin-1.2.3/config/config.php", PhpldapadminResources.class.getResource("config_php_expected.txt")),
+
+	static copyUbuntuFiles(File parent) {
+	}
+
+	ResourcesUtils resources
+
+	PhpldapadminResources(String path, URL resource) {
+		this.resources = new ResourcesUtils(path: path, resource: resource)
+	}
+
+	String getPath() {
+		resources.path
+	}
+
+	URL getResource() {
+		resources.resource
+	}
+
+	File asFile(File parent) {
+		resources.asFile parent
+	}
+
+	void createFile(File parent) {
+		resources.createFile parent
+	}
+
+	void createCommand(File parent) {
+		resources.createCommand parent
+	}
+
+	String replaced(File parent, def search, def replace) {
+		resources.replaced parent, search, replace
+	}
+
+	String toString() {
+		resources.toString()
+	}
+}
---
+++ sscontrol-httpd-apache/src/test/groovy/com/anrisoftware/sscontrol/httpd/apache/phpldapadmin/ubuntu_10_04/PhpldapadminTest.groovy
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of sscontrol-httpd-apache.
+ *
+ * sscontrol-httpd-apache is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-httpd-apache is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-httpd-apache. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.sscontrol.httpd.apache.phpldapadmin.ubuntu_10_04
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static com.anrisoftware.sscontrol.httpd.apache.core.ubuntu_10_04.UbuntuResources.*
+import static com.anrisoftware.sscontrol.httpd.apache.phpldapadmin.ubuntu_10_04.PhpldapadminResources.*
+import static org.apache.commons.io.FileUtils.*
+import groovy.util.logging.Slf4j
+
+import org.junit.Test
+
+import com.anrisoftware.sscontrol.httpd.apache.ubuntu.UbuntuTestUtil
+
+/**
+ * Test Apache on a Ubuntu 10.04 server.
+ *
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+@Slf4j
+class PhpldapadminTest extends UbuntuTestUtil {
+
+	@Test
+	void "phpldapadmin"() {
+		copyUbuntuFiles tmpdir
+
+		loader.loadService profile.resource, null
+		def profile = registry.getService("profile")[0]
+		loader.loadService httpdScript.resource, profile
+
+		registry.allServices.each { it.call() }
+		log.info "Run service again to ensure that configuration is not set double."
+		registry.allServices.each { it.call() }
+
+		assertFileContent defaultConf.asFile(tmpdir), defaultConf
+		assertFileContent domainsConf.asFile(tmpdir), PhpldapadminResources.domainsConf
+		assertStringContent test1comConf.replaced(tmpdir, tmpdir, "/tmp"), test1comConf.toString()
+		assertStringContent test1comSslConf.replaced(tmpdir, tmpdir, "/tmp"), test1comSslConf.toString()
+		assertStringContent ldapadminTest1comSslConf.replaced(tmpdir, tmpdir, "/tmp"), ldapadminTest1comSslConf.toString()
+		assertFileContent ldapadminTest1comSslFcgiScript.asFile(tmpdir), ldapadminTest1comSslFcgiScript
+		assertStringContent chownOut.replaced(tmpdir, tmpdir, "/tmp"), chownOut.toString()
+		assertStringContent chmodOut.replaced(tmpdir, tmpdir, "/tmp"), chmodOut.toString()
+		assert phpldapadminTgz.asFile(tmpdir).isFile()
+		assertFileContent exampleConfig.asFile(tmpdir), exampleConfig
+		assertFileContent linkedExampleConfig.asFile(tmpdir), linkedExampleConfig
+		assertFileContent expectedConfig.asFile(tmpdir), expectedConfig
+	}
+}

04df08920a95ba8dfaba53e8f9f3773b09c4a374
Erwin Mueller <erwin.mueller@deventm.org>
2013-09-14 12:45:30 +0200

    Fix hash storage test
    #2670

--- sscontrol-mail-postfix/src/test/groovy/com/anrisoftware/sscontrol/mail/postfix/hashstorage/ubuntu_10_04/separate_domains_nonunix_accounts/HashPostfixTest.groovy
+++ sscontrol-mail-postfix/src/test/groovy/com/anrisoftware/sscontrol/mail/postfix/hashstorage/ubuntu_10_04/separate_domains_nonunix_accounts/HashPostfixTest.groovy
@@ -56,6 +56,6 @@ class HashPostfixTest extends UbuntuTestUtil {
 		assertFileContent aliasMapsExpected.asFile(tmpdir), aliasMapsExpected
 		assertFileContent mailboxMapsExpected.asFile(tmpdir), mailboxMapsExpected
 		assertStringContent chownOut.replaced(tmpdir, tmpdir, "/tmp"), chownOut.toString()
-		assert virtualMailboxBaseDir.asFile(tmpdir).isDirectory()
+		assert mailboxBaseDir.asFile(tmpdir).isDirectory()
 	}
 }

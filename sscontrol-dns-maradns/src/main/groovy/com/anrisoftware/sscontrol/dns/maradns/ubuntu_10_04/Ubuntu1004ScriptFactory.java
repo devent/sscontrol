5186438e63681ab383f44813740aec9b8234eb14
Erwin Mueller <erwin.mueller@deventm.org>
2013-09-29 19:00:06 +0200

    Update license headers

--- sscontrol-dns-maradns/src/main/groovy/com/anrisoftware/sscontrol/dns/maradns/ubuntu_10_04/Ubuntu1004ScriptFactory.java
+++ sscontrol-dns-maradns/src/main/groovy/com/anrisoftware/sscontrol/dns/maradns/ubuntu_10_04/Ubuntu1004ScriptFactory.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
- * 
+ *
  * This file is part of sscontrol-dns-maradns.
- * 
- * sscontrol-dns-maradns is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
+ *
+ * sscontrol-dns-maradns is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * sscontrol-dns-maradns is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with sscontrol-dns-maradns. If not, see <http://www.gnu.org/licenses/>.
  */

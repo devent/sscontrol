270c492358355a2a1c6c1927d28e50661f4682af
Erwin Mueller <erwin.mueller@deventm.org>
2013-12-15 16:06:18 +0100

    Update license headers
    #2757

--- sscontrol-core/src/main/java/com/anrisoftware/sscontrol/core/debuglogging/DebugLoggingModule.java
+++ sscontrol-core/src/main/java/com/anrisoftware/sscontrol/core/debuglogging/DebugLoggingModule.java
@@ -1,20 +1,20 @@
 /*
- * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
+ * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of sscontrol-database.
+ * This file is part of sscontrol-core.
  *
- * sscontrol-database is free software: you can redistribute it and/or modify it
+ * sscontrol-core is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * sscontrol-database is distributed in the hope that it will be useful, but
+ * sscontrol-core is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with sscontrol-database. If not, see <http://www.gnu.org/licenses/>.
+ * along with sscontrol-core. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.core.debuglogging;
 

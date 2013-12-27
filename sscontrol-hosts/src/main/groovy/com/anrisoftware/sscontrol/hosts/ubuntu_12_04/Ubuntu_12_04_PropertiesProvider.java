d60db501fa8f3e7d455e4a5ed3f69f8cd49a5e15
Erwin Mueller <erwin.mueller@deventm.org>
2013-12-27 02:47:30 +0100

    Add Ubuntu 12.04 hosts service
    #2765

---
+++ sscontrol-hosts/src/main/groovy/com/anrisoftware/sscontrol/hosts/ubuntu_12_04/Ubuntu_12_04_PropertiesProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of sscontrol-hosts.
+ *
+ * sscontrol-hosts is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-hosts is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-hosts. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.sscontrol.hosts.ubuntu_12_04;
+
+import java.net.URL;
+
+import com.anrisoftware.propertiesutils.AbstractContextPropertiesProvider;
+
+/**
+ * Provides the Hosts/Ubuntu 12.04 properties from
+ * {@code hosts_ubuntu_12_04.properties}.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+@SuppressWarnings("serial")
+public class Ubuntu_12_04_PropertiesProvider extends
+		AbstractContextPropertiesProvider {
+
+	private static final URL RESOURCE = Ubuntu_12_04_PropertiesProvider.class
+            .getResource("/hosts_ubuntu_12_04.properties");
+
+	Ubuntu_12_04_PropertiesProvider() {
+		super(Ubuntu_12_04_PropertiesProvider.class, RESOURCE);
+	}
+
+}
---
+++ sscontrol-hosts/src/test/groovy/com/anrisoftware/sscontrol/hosts/ubuntu_12_04/HostsResources.groovy
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of sscontrol-hosts.
+ *
+ * sscontrol-hosts is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-hosts is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-hosts. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.sscontrol.hosts.ubuntu_12_04
+
+import static com.anrisoftware.globalpom.utils.TestUtils.*
+import static org.apache.commons.io.FileUtils.*
+
+import com.anrisoftware.sscontrol.hosts.resources.ResourcesUtils
+
+/**
+ * Hosts/Ubuntu resources.
+ *
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+enum HostsResources {
+
+    profile("UbuntuProfile.groovy", HostsResources.class.getResource("UbuntuProfile.groovy")),
+    hostsService("Hosts.groovy", HostsResources.class.getResource("HostsService.groovy")),
+    hostsExpected("/etc/hosts", HostsResources.class.getResource("hosts_expected.txt")),
+    hostsFile("/etc/hosts", HostsResources.class.getResource("hosts.txt")),
+
+    ResourcesUtils resources
+
+    HostsResources(String path, URL resource) {
+        this.resources = new ResourcesUtils(path: path, resource: resource)
+    }
+
+    String getPath() {
+        resources.path
+    }
+
+    URL getResource() {
+        resources.resource
+    }
+
+    File asFile(File parent) {
+        resources.asFile parent
+    }
+
+    void createFile(File parent) {
+        resources.createFile parent
+    }
+
+    void createCommand(File parent) {
+        resources.createCommand parent
+    }
+
+    String replaced(File parent, def search, def replace) {
+        resources.replaced parent, search, replace
+    }
+
+    String toString() {
+        resources.toString()
+    }
+}

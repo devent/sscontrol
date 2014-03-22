48883dc7272395b20724d5df3ce60e1c33cef82d
Erwin Mueller <erwin.mueller@deventm.org>
2014-03-22 13:51:51 +0100

    Fix errors for httpd-gitit.

--- sscontrol-httpd-gitit/src/main/java/com/anrisoftware/sscontrol/httpd/gitit/GititServiceProvider.java
+++ sscontrol-httpd-gitit/src/main/java/com/anrisoftware/sscontrol/httpd/gitit/GititServiceProvider.java
@@ -1,20 +1,20 @@
 /*
- * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
+ * Copyright 2012-2014 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of sscontrol-httpd.
+ * This file is part of sscontrol-httpd-gitit.
  *
- * sscontrol-httpd is free software: you can redistribute it and/or modify it
+ * sscontrol-httpd-gitit is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * sscontrol-httpd is distributed in the hope that it will be useful, but
+ * sscontrol-httpd-gitit is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with sscontrol-httpd. If not, see <http://www.gnu.org/licenses/>.
+ * along with sscontrol-httpd-gitit. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.sscontrol.httpd.gitit;
 
